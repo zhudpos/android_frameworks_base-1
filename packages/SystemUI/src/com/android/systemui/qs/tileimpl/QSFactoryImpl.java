b90573a56c2db6e4c9434eadb8602d73ba5b7092
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-09-26 05:04:03 +0200

    QS: Add Expanded Desktop QS Tile
    Change-Id: Ib6ea6e3cba9f26de8d97c2eb5cda9728365fd077
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    QS: Expanded Desktop Tile Reloaded
    
    Now switches between full, off and if you have a navbar enabled also statusbar and navbar
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -31,6 +31,7 @@ import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
+import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
@@ -89,6 +90,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("heads_up")) return new HeadsUpTile(mHost);
         else if (tileSpec.equals("weather")) return new WeatherTile(mHost);
         else if (tileSpec.equals("ambient")) return new AmbientLiftToWakeTile(mHost);
+        else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
