0ac697ff01ea0fb009bc44562738c9897bc50154
Harsh Shandilya <msfjarvis@gmail.com>
2017-10-12 13:33:45 +0000

    SystemUI: Disable emergency affordance
    * Inspired by Men_In_Black007's patch for this from Nougat.
    
    Change-Id: I6daf2e3f73fef91d936300961dd476da51d0cc4e
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -442,10 +442,6 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             addedKeys.add(actionKey);
         }
 
-        if (mEmergencyAffordanceManager.needsEmergencyAffordance()) {
-            mItems.add(getEmergencyAction());
-        }
-
         mAdapter = new MyAdapter();
 
         AlertParams params = new AlertParams(mContext);
