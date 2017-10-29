f838e9fc563910d3ff34b439b153b3f369b8b0b1
Simao Gomes Viana <xdevs23@outlook.com>
2017-10-29 13:14:00 +0100

    AudioService: disable safe media volume
    Change-Id: I4f0dc129ae364a18c0ca85bb0ea50339f82585d1

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -688,9 +688,7 @@ public class AudioService extends IAudioService.Stub
                 null,
                 0);
 
-        mSafeMediaVolumeState = new Integer(Settings.Global.getInt(mContentResolver,
-                                                        Settings.Global.AUDIO_SAFE_VOLUME_STATE,
-                                                        SAFE_MEDIA_VOLUME_NOT_CONFIGURED));
+        mSafeMediaVolumeState = SAFE_MEDIA_VOLUME_DISABLED;
         // The default safe volume index read here will be replaced by the actual value when
         // the mcc is read by onConfigureSafeVolume()
         mSafeMediaVolumeIndex = mContext.getResources().getInteger(
