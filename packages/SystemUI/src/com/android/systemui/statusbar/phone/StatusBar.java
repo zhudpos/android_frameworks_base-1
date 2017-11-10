2df7d5b528f79d5e3d679407246c7893a07821c3
Dabug123 <clarkbrunswick55@gmail.com>
2017-11-10 04:56:04 +0100

    missing from status bar logo thanks max and martin for the look at your addition

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5795,6 +5795,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
             update();
         }
+		
+		@Override
+		    public void onChange(boolean selfChange) {
+		}
 
         public void update() {
             setDoubleTapNavbar();
